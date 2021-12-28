ba84c164461aae4d73ff6bbf41a503084fe350d3
takumifukasawa <takumifukasawa@gmail.com>
2021-12-28 23:29:08 +0900

    kdtree側に移す

--- KdTree.js
+++ KdTree.js
@@ -9,17 +9,47 @@ class Node {
 
 export default class KdTree {
     #root;
-    constructor(pointList, axes, medianFunc) {
-        const iter = KdTree.iter(pointList, axes, medianFunc, 0);
+
+    constructor(pointList, dimension) {
+        const axes = ["x", "y", "z"].splice(0, dimension);
+        const iter = KdTree.iter(pointList, axes, 0);
         this.#root = iter;
     }
-    static iter(list, axes, medianFunc, depth) {
+
+    static medianFunc(list, axes, currentAxis) {
+        const sorted = [...list].sort((v1, v2) => {
+            return v1[currentAxis] - v2[currentAxis];
+        });
+        const isOdd = sorted.length % 2 === 0;
+        if(!isOdd) {
+            const index = Math.floor(sorted.length / 2)
+            return [
+                sorted[index],
+                sorted,
+                index
+            ];
+        }
+        const baseIndex = sorted.length / 2;
+        const m1 = sorted[baseIndex - 1];
+        const m2 = sorted[baseIndex];
+        const medianValue = axes.reduce((prev, current, v) => {
+            prev[current] = (m1[current] + m2[current]) / 2;
+            return prev;
+        }, {});
+        return [
+            medianValue,
+            sorted,
+            baseIndex - 0.5
+        ];
+    }
+
+    static iter(list, axes, depth) {
         if(list.length < 1) {
             return null;
         }
         const axisIndex = depth % axes.length;
         const axis = axes[axisIndex];
-        const [median, sortedList] = medianFunc(list, axis);
+        const [median, sortedList] = KdTree.medianFunc(list, axes, axis);
         const beforeList = sortedList.splice(
             0,
             sortedList.length % 2 == 0
@@ -31,12 +61,12 @@ export default class KdTree {
         if(beforeList.length === 1) {
             node.leftChild = new Node(beforeList[0]);
         } else {
-            node.leftChild = KdTree.iter(beforeList, axes, medianFunc, depth + 1);
+            node.leftChild = KdTree.iter(beforeList, axes, depth + 1);
         }
         if(afterList.length === 1) {
             node.rightChild = new Node(afterList[0]);
         } else {
-            node.rightChild = KdTree.iter(afterList, axes, medianFunc, depth + 1);
+            node.rightChild = KdTree.iter(afterList, axes, depth + 1);
         }
         return node;
     }
