9076b5c1cbb35c67b740c9a234bcfb3309c3e1d3
takumifukasawa <takumifukasawa@gmail.com>
2021-12-29 21:04:58 +0900

    fix instance values

--- KdTree.js
+++ KdTree.js
@@ -1,24 +1,24 @@
 class Node {
-    value = null;
-    axis = null;
-    leftChild = null;
-    rightChild = null;
-    constructor(value, axis) {
-        this.value = value;
-        this.axis = axis;
+    splittingValue = null; // 中間nodeの時分割値を保存
+    point = null; // 葉のときに値を保存
+    axis = null; // 中間nodeの時に分割軸を保存
+    leftChild = null; // 中間nodeの時に左の子を保存
+    rightChild = null; // 中間nodeの時に左の子を保存
+    constructor() {
     }
 }
 
 export default class KdTree {
+    #axes;
     #root;
 
     constructor(pointList) {
-        const axes = Object.getOwnPropertyNames(pointList[0]);
-        const iter = KdTree.iter(pointList, axes);
+        this.#axes = Object.getOwnPropertyNames(pointList[0]);
+        const iter = KdTree.iter(pointList, this.#axes);
         this.#root = iter;
     }
 
-    static medianFunc(list, axes, currentAxis) {
+    static medianFunc(list, currentAxis, axes) {
         const sorted = [...list].sort((v1, v2) => {
             return v1[currentAxis] - v2[currentAxis];
         });
@@ -46,7 +46,7 @@ export default class KdTree {
     static iter(list, axes, depth = 0) {
         const axisIndex = depth % axes.length;
         const axis = axes[axisIndex];
-        const [median, sortedList] = KdTree.medianFunc(list, axes, axis);
+        const [median, sortedList] = KdTree.medianFunc(list, axis, axes);
         const beforeList = sortedList.splice(
             0,
             sortedList.length % 2 == 0
@@ -54,14 +54,16 @@ export default class KdTree {
                 : Math.ceil(sortedList.length / 2)
         );
         const afterList = sortedList;
-        const node = new Node(median, axis);
-        console.log("==============");
-        console.log(median, list, beforeList, afterList)
+        const node = new Node();
+        node.splittingValue = median;
+        node.axis = axis;
         if(beforeList.length === 0) {
             // NOTE: 0 のときにわざわざこの処理をする必要はなさそう
             node.leftChild = null;
         } else if(beforeList.length === 1) {
-            node.leftChild = new Node(beforeList[0]);
+            const leftNode = new Node();
+            leftNode.point = beforeList[0];
+            node.leftChild = leftNode;
         } else {
             node.leftChild = KdTree.iter(beforeList, axes, depth + 1);
         }
@@ -69,10 +71,18 @@ export default class KdTree {
             // NOTE: 0 のときにわざわざこの処理をする必要はなさそう
             node.rightChild = null;
         } else if(afterList.length === 1) {
-            node.rightChild = new Node(afterList[0]);
+            const rightNode = new Node();
+            rightNode.point = afterList[0];
+            node.rightChild = rightNode;
         } else {
             node.rightChild = KdTree.iter(afterList, axes, depth + 1);
         }
         return node;
+    }
+
+    findArea(targetPoint) {
+        const find = (currentNode, target) => {
+        }
+        find(this.#axes, target);
     }
 }
